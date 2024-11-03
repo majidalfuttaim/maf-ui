1e1f9db27c80c74648bbc8093c2e98f2ab219914
MAbuseini <mahmoud.abuseini@maf.ae>
2024-11-03 15:18:39 +0300

    updated navigation components

--- src/components/mf-navigation/mf-navigation.styles.ts
+++ src/components/mf-navigation/mf-navigation.styles.ts
@@ -2,10 +2,25 @@ import { css } from 'lit';
 
 export default css`
   :host {
-    display: block;
+    position: relative;
+    background: var(--mf-navigation-background-color);
+    border: solid var(--mf-navigation-border-width) var(--mf-navigation-border-color);
+    border-radius: var(--mf-navigation-radius-medium);
+    padding: var(--mf-navigation-spacing-x-small) 0;
+    overflow: auto;
+    overscroll-behavior: none;
+  }
+
+  ::slotted(sl-divider) {
+    --spacing: var(--mf-navigation-spacing-x-small);
   }
   .navigation {
     display: flex;
     border-radius: 0;
+    flex-direction: row;
+  }
+
+  .navigation.navigation--vertical {
+    flex-direction: column;
   }
 `;
